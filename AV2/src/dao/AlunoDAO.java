9cdee7a3eb6246fd1d9848efc53a2077845373c3
David Sena <daviidsena@gmail.com>
2022-07-01 00:30:53 -0300

    Update AlunoDAO with throws

--- AV2/src/dao/AlunoDAO.java
+++ AV2/src/dao/AlunoDAO.java
@@ -1,13 +1,15 @@
 package dao;
 
+import java.sql.SQLException;
+
 import model.Aluno;
 
 public interface AlunoDAO {
-	Aluno obterAluno(int IdAluno);
+	Aluno obterAluno(int IdAluno) throws SQLException;
 
-	void salvarAluno(Aluno aluno);
+	void salvarAluno(Aluno aluno) throws SQLException;
 
-	void atualizarAluno(Aluno aluno);
+	void atualizarAluno(Aluno aluno) throws SQLException;
 
-	void deletarAluno(int AlunoId);
+	void deletarAluno(int AlunoId) throws SQLException;
 }
