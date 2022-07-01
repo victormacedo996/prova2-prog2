4132ef087bcf0eb616169461c4a98da398aa8442
David Sena <daviidsena@gmail.com>
2022-07-01 00:07:18 -0300

    Add entity Aluno

---
+++ AV2/src/model/Aluno.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Aluno {
+	private int Id;
+	private String Nome;
+	private String CPF;
+	private String DataNascimento;
+	private String Naturalidade;
+	private String Endereco;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public String getDataNascimento() {
+		return DataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		DataNascimento = dataNascimento;
+	}
+	public String getNaturalidade() {
+		return Naturalidade;
+	}
+	public void setNaturalidade(String naturalidade) {
+		Naturalidade = naturalidade;
+	}
+	public String getEndereco() {
+		return Endereco;
+	}
+	public void setEndereco(String endereco) {
+		Endereco = endereco;
+	}
+}
