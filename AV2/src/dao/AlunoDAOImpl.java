c117a2689a77b6d08301e45ec5b80a58d9f92bc7
David Sena <daviidsena@gmail.com>
2022-07-01 00:46:22 -0300

    Add salvarAluno in AlunoDAO

---
+++ AV2/src/dao/AlunoDAOImpl.java
@@ -0,0 +1,53 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import factory.ConnectionFactory;
+import model.Aluno;
+
+public class AlunoDAOImpl implements AlunoDAO {
+
+	private final Connection con;
+
+	public AlunoDAOImpl() throws SQLException {
+		this.con = ConnectionFactory.getConnection();
+	}
+
+	@Override
+	public Aluno obterAluno(int IdAluno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void salvarAluno(Aluno aluno) throws SQLException {
+		String query = "INSERT INTO alunos (NOME, CPF, DATA_NASCIMENTO, NATURALIDADE, ENDERECO) VALUES (?, ?, ?, ?, ?)";
+		PreparedStatement stmt = con.prepareStatement(query);
+		
+		stmt.setString(1, aluno.getNome());
+		stmt.setString(2, aluno.getCPF());
+		stmt.setString(3, aluno.getDataNascimento());
+		stmt.setString(4, aluno.getNaturalidade());
+		stmt.setString(5, aluno.getEndereco());
+		
+		stmt.execute();
+		
+		stmt.close();
+		con.close();
+	}
+
+	@Override
+	public void atualizarAluno(Aluno aluno) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deletarAluno(int AlunoId) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
