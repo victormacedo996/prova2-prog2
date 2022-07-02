ba50a2aacdef3081ecf0f2f2df62cafde3dbd130
David Sena <daviidsena@gmail.com>
2022-07-01 22:17:37 -0300

    Update AlunoDAO

--- AV2/src/dao/AlunoDAO.java
+++ AV2/src/dao/AlunoDAO.java
@@ -7,9 +7,9 @@ import model.Aluno;
 public interface AlunoDAO {
 	Aluno obterAluno(int IdAluno) throws SQLException;
 
-	void salvarAluno(Aluno aluno) throws SQLException;
+	boolean salvarAluno(Aluno aluno) throws SQLException;
 
-	void atualizarAluno(Aluno aluno) throws SQLException;
+	boolean atualizarAluno(Aluno aluno) throws SQLException;
 
-	void deletarAluno(int AlunoId) throws SQLException;
+	boolean deletarAluno(int AlunoId) throws SQLException;
 }
--- AV2/src/dao/AlunoDAOImpl.java
+++ AV2/src/dao/AlunoDAOImpl.java
@@ -1,8 +1,12 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import factory.ConnectionFactory;
 import model.Aluno;
@@ -15,46 +19,102 @@ public class AlunoDAOImpl implements AlunoDAO {
 		this.con = ConnectionFactory.getConnection();
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
-	public Aluno obterAluno(int IdAluno) {
+	public Aluno obterAluno(int IdAluno) throws SQLException {
 		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT id, nome, cpf, data_nascimento, naturalidade, endereco FROM alunos";
+		PreparedStatement stmt = con.prepareStatement(query);
+		ResultSet rs = stmt.executeQuery();
+		
+		Aluno aluno = null;
+		
+		while (rs.next()) {
+			if(rs.getInt(1) == IdAluno) {
+				aluno = new Aluno();
+				aluno.setId(rs.getInt("id"));
+				aluno.setNome(rs.getString("nome"));
+				aluno.setCPF(rs.getString("cpf"));
+				
+				Date data = rs.getDate("data_nascimento");
+				Calendar dataNascimento = new GregorianCalendar();
+				dataNascimento.setTime(data);
+				aluno.setDataNascimento(dataNascimento);
+				aluno.setNaturalidade(rs.getString("naturalidade"));
+				aluno.setEndereco(rs.getString("endereco"));
+			};
+		}
+		
+		rs.close();
+		stmt.close();
+		con.close();
+		
+		return aluno;
 	}
 
 	@Override
-	public void salvarAluno(Aluno aluno) throws SQLException {
+	public boolean salvarAluno(Aluno aluno) throws SQLException {
+		boolean result = false;
 		String query = "INSERT INTO alunos (NOME, CPF, DATA_NASCIMENTO, NATURALIDADE, ENDERECO) VALUES (?, ?, ?, ?, ?)";
 		PreparedStatement stmt = con.prepareStatement(query);
 		
-		stmt.setString(1, aluno.getNome());
+		Calendar data = aluno.getDataNascimento();
+        Date dataFinal = new Date(data.getTimeInMillis());
+        
+ 		stmt.setString(1, aluno.getNome());
 		stmt.setString(2, aluno.getCPF());
-		stmt.setString(3, aluno.getDataNascimento());
+		stmt.setDate(3, dataFinal);
 		stmt.setString(4, aluno.getNaturalidade());
 		stmt.setString(5, aluno.getEndereco());
 		
-		stmt.execute();
+		result = stmt.executeUpdate() > 0;
 		
 		stmt.close();
 		con.close();
+		
+		return result;
 	}
 
 	@Override
-	public void atualizarAluno(Aluno aluno) {
+	public boolean atualizarAluno(Aluno aluno) throws SQLException {
 		// TODO Auto-generated method stub
-
+		boolean result = false;
+		
+		String query = "UPDATE alunos SET NOME = ?, CPF = ?, DATA_NASCIMENTO = ?, NATURALIDADE = ?, ENDERECO = ? WHERE id = ?";
+		PreparedStatement stmt = con.prepareStatement(query);
+		
+		Calendar data = aluno.getDataNascimento();
+        Date dataFinal = new Date(data.getTimeInMillis());
+        
+		stmt.setString(1, aluno.getNome());
+		stmt.setString(2, aluno.getCPF());
+		stmt.setDate(3, dataFinal);
+		stmt.setString(4, aluno.getNaturalidade());
+		stmt.setString(5, aluno.getEndereco());
+		stmt.setInt(6, aluno.getId());
+		
+		result = stmt.executeUpdate() > 0;
+		
+		stmt.close();
+		con.close();
+		
+		return result;
 	}
 
 	@Override
-	public void deletarAluno(int AlunoId) throws SQLException {
+	public boolean deletarAluno(int AlunoId) throws SQLException {
 		// TODO Auto-generated method stub
+		boolean result = false;
 		String query = "DELETE FROM alunos WHERE id = ?";
 		PreparedStatement stmt = con.prepareStatement(query);
 		
 		stmt.setInt(1, AlunoId);
 		
-		stmt.execute();
+		result = stmt.executeUpdate() > 0;
 		stmt.close();
 		con.close();
+		
+		return result;
 	}
 
 }
